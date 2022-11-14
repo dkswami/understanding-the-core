cf18c67113a3c935496320178d4efd54b49f3748
dkswami <dkswami1820@gmail.com>
2022-11-14 16:01:19 +0530

    added body parser

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes)
-server.listen(3001);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/add-product', (req, res, next) => {
+	res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+})
+
+app.use('/product', (req, res, next) => {
+	console.log(req.body);
+	res.redirect('/');
+})
+
+app.get('/show', (req, res, next) => {
+	res.send('<h1>showing from Express!</h1>');
+})
+
+app.use('/', (req, res, next) => {
+	res.send('<h1>Hello from Express!</h1>');
+	next();
+})
+
+app.listen(3002);
